70c76f1b47e341c17966e4f0b69b079b1ba56c8f
inseok1121 <inseck1121@gmail.com>
2019-05-25 23:53:15 +0900

    first commit

---
+++ web1/main.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var fs = require('fs');
+
+var app = http.createServer(function(request, response){
+  var url = request.url;
+  if(request.url == '/'){
+    url = '/index.html';
+  }
+
+  if(request.url == '/favicon.ico'){
+    return response.writeHead(404);
+    response.end()
+    return;
+  }
+
+  response.writeHead(200);
+  console.log(__dirname + url)
+
+  response.end("Test");
+});
+
+app.listen(3000);
